ff2926a47240d4867f6e8e224e4ee163ae9bc0b2
Aayush501 <shrivastavashu501@gmail.com>
2025-02-28 16:33:52 +0530

    [Aayush Kumar Shrivastav] Add . Added Pattern To Name

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ public class EmployeeDTO {
     private long ID;
 
     @NotBlank(message = "Name Cannot Be Blank!!")
+    @Size(min = 2, max = 25, message = "name must be between 2 and 25 characters.")
     private String name;
     private double salary;
 }
