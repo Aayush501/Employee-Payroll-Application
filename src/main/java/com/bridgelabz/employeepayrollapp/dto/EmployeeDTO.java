7c767bfc464be1dffb436f2d0680d36ee6da2c79
Aayush501 <shrivastavashu501@gmail.com>
2025-02-28 16:29:59 +0530

    [Aayush Kumar Shrivastav] Add . maked name a required field

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,8 @@ import lombok.Setter;
 @Getter
 public class EmployeeDTO {
     private long ID;
+
+    @NotBlank(message = "Name Cannot Be Blank!!")
     private String name;
     private double salary;
 }
