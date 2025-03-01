d988905dcf6019d1858c1033098bcd84b24e556a
Aayush501 <shrivastavashu501@gmail.com>
2025-03-01 15:38:06 +0530

    [Aayush Kumar Shrivastav] Add . Section5-UC2 -> Ensure validations is done on the Payroll DTO -> solution

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,22 +1,37 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Setter
 @Getter
 public class EmployeeDTO {
-    private long ID;
 
     @NotBlank(message = "Name Cannot Be Blank!!")
     @Size(min = 2, max = 25, message = "name must be between 2 and 25 characters.")
     private String name;
+
+    @Min(value = 500, message = "Salary should not be less than 500")
     private double salary;
+
+    @Pattern(regexp = "male|female", message = "gender can only be male or female")
     private String gender;
-    private String startDate;
+
+    @JsonFormat (pattern = "dd MM yyyy")
+    @NotNull (message = "startDate Cannot Be Empty")
+    @PastOrPresent (message = "Date Should Be Today Or Past")
+    private LocalDate startDate;
+
+    @NotBlank (message = "Note Cannot Be Blank")
     private String note;
+
+    @NotBlank (message = "Profile Pic Cannot Be Blank")
     private String profilePic;
+
+    @NotBlank (message = "Department Pic Cannot Be Blank")
     private String department;
 }
--- src/main/java/com/bridgelabz/employeepayrollapp/exception/ExceptionHandlers.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exception/ExceptionHandlers.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.employeepayrollapp.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionHandlers {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException m) {
